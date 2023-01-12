6c17614e49d2b9a8425649d2a19a1cb3cae83137
Mayar Hussein <mayar_memoo@ymail.com>
2023-01-12 01:36:36 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow,Grid} from '@mui/material'
+import disneyland from './images/disneyland.jpg';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+
+  const classes = useStyles();
+ 
     return(
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+       <Container maxWidth="lg">
+           <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Disney Characters</Typography>
+                <img className={classes.image} src={disneyland} alt="memories" height="60" />
+           </AppBar>
+           <Grow in>
+             <Container>
+                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                           <Grid item xs={12} sm={7}>
+                                <Posts/>   
+                           </Grid>
+                           <Grid item xs={12} sm={4}>
+                                <Form/>
+                           </Grid>
+                     </Grid>
+             </Container>
+            </Grow>         
+       </Container> 
+     )
 
 }
 
---
+++ src/components/Posts/styles.js
@@ -0,0 +1,15 @@
+import { styled } from '@mui/system';
+
+
+export default styled((theme) => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
